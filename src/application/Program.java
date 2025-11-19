dea9f91b0b2879f8718e22de83e35d4894b391de
William Rodrigues <rodriw01@outlook.com>
2025-11-19 00:47:37 -0300

    Getting path files

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,26 +8,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
-		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS: ");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES: ");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		
-		Boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created successfully: " + success);
-		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 		
 		sc.close();
 			
