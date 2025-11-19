eee493003b6ee21efcffe0e326af3cbab01beed9
William Rodrigues <rodriw01@outlook.com>
2025-11-19 00:40:15 -0300

    Working with Directories and Files

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,35 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
-			String[] lines = new String[] { "Good morning", 
-											"Good afternoom",
-											"Goof nigth"};
-			
-			String path = "c:\\temp\\out.txt";
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
-				for (String line : lines ) {					
-					bw.write(line);
-				    bw.newLine();				
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES: ");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		Boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		
+		sc.close();
 			
 		}
 
