c05b7377cf6a86b732b3f46fd36687427697008a
William Rodrigues <rodriw01@outlook.com>
2025-11-19 00:25:13 -0300

    Writing in files

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,28 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		String path = "c:\\temp\\in.txt";
-			
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line = br.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-		}
-		catch (IOException e){
-			System.out.println("Error: " + e.getMessage());
-		}
-		
-	}
 	
+			String[] lines = new String[] { "Good morning", 
+											"Good afternoom",
+											"Goof nigth"};
+			
+			String path = "c:\\temp\\out.txt";
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
+				for (String line : lines ) {					
+					bw.write(line);
+				    bw.newLine();				
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+
 }
